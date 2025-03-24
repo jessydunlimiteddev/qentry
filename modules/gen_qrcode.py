09fb022609d14be157d40628fbdbb98c98cba913
jessyunlimiteddev <unlimitedtv0@gmail.com>
2025-03-24 09:07:55 -0700

    url

--- modules/gen_qrcode.py
+++ modules/gen_qrcode.py
@@ -3,7 +3,7 @@ import qrcode
 from io import BytesIO
 
 def generate_qr():
-    url = "https://qentry.streamlit.app/checkin_page"  
+    url = "https://qentryy.streamlit.app/checkin_page"  
     qr = qrcode.make(url)
     buf = BytesIO()
     qr.save(buf, format="PNG")
